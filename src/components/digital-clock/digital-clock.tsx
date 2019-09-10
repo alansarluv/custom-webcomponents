6baa26ca6b1e72b17e7ef160de52142cb44b466e
alansarluv <alan.sarluf@gmail.com>
2019-09-10 20:05:13 +0700

    Update js for digital clock

--- src/components/digital-clock/digital-clock.tsx
+++ src/components/digital-clock/digital-clock.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
 
 @Component({
   tag: 'cwc-digital-clock',
@@ -7,21 +7,32 @@ import { Component, h } from '@stencil/core';
 })
 
 export class DigitalClock {
-  
+    @State() timeValSec = 0;
+    @State() timeValMin = 0;
+    @State() timeValHour = 0;
+
+  componentDidLoad() {
+    setInterval(() => this.runClock(new Date()), 1000);
+  }
+  runClock(val) {
+    this.timeValSec = val.getSeconds() * 6;
+    this.timeValMin = val.getMinutes() * 6;
+    this.timeValHour = val.getHours() * 30;
+  }
   render() {
     return [
       <div id="cwc-clock_wrap">
-        <div class="cwc-clock-rotatingWrapper">
+        <div class="cwc-clock-rotatingWrapper" style={{'transform': 'rotate('+this.timeValHour+'deg)'}}>
           <span class="cwc-hour-pointer"></span>
         </div>
-        <div class="cwc-clock-rotatingWrapper">
+        <div class="cwc-clock-rotatingWrapper" style={{'transform': 'rotate('+this.timeValMin+'deg)'}}>
           <span class="cwc-min-pointer"></span>
         </div>
-        <div class="cwc-clock-rotatingWrapper">
+        <div class="cwc-clock-rotatingWrapper" style={{'transform': 'rotate('+this.timeValSec+'deg)'}}>
           <span class="cwc-sec-pointer"></span>
         </div>
         <div class="cwc-clock-base"></div>
-      </div>      
+      </div>
     ];
   }
 }
